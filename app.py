990d00c7b18d7f8186192b66e00f02676db8de28
Jake <nhnam6@gmail.com>
2026-09-09 19:29:18 +0700

    fix: this is broken on purpose (#6)
    * fix: this is broken on purpose
    
    * fix: revert the deliberate breakage

--- app.py
+++ app.py
@@ -1,6 +1,1 @@
-"""Trivial app so CI has something real to run. Not the point of the demo."""
-
-
-def add(a: int, b: int) -> int:
-    return a + b
-def pending(): pass
+def add(a, b): return a + b
